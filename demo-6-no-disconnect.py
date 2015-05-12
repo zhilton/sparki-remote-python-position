4d183a8f1573c0c4a96585a64e474f91f92fc25c
Zach Hilton <machao322@users.noreply.github.com>
2015-05-12 09:27:21 +0200

    Fixed loop in demo-6-no-threading.py

--- demo-6-no-disconnect.py
+++ demo-6-no-disconnect.py
@@ -29,35 +29,41 @@ def generateInitGoals( number ):
 		goals.put((randX, randY),True)
 
 """
+Run the passed function on the passed Sparki process
+"""
+def read_msg(sparki, data):
+    funcname, args = data
+    func = getattr(sparki, funcname)
+    func(*args)
+
+"""
+Start a loop for a Sparki process
+"""
+def loopFunct(q, sparki):
+    if( not sparki.connect()):
+		print "Sparki " + sparki.portName + " failed to connect"
+		exit()
+    while True:
+        try:
+            message = q.get()
+            read_msg(sparki, (message[0], message[1]))
+        except Empty:
+            pass
+    sparki.disconnect
+
+"""
 Replace with the code to go to a goal with Sparki
 """
-def assignTask(goal, sparki):
-	#################################################################v
-	# Replace with what you want Sparki to do
-	# Don't forget to generate new goals throughout this function in some way
-	# likely by just adding goals at all spots seen in the sweep
-	if( not sparki.connect()):
-		print "Sparki " + sparki.portName + " failed to connect"
-		goals.put(goal, True)
-		return
-
-	sparki.goTo(goal[0],goal[1])
+def assignGoal(goal, sparkiQ):
+	sparkiQ.put( ('goTo', (goal[0],goal[1],)), True )
 
 	randX = (random.random() * MAX_GRID_SIZE)
 	randY = (random.random() * MAX_GRID_SIZE)
 
 	new = (randX, randY)
-
-	print goal
 	#### Add Goals as found ####
 	goals.put(new, True)
-	
-	#################################################################^
-
-	# Free Sparki again
-	sparki.disconnect()
-	sparkis.put(sparki, True)
-	
+	sparkis.put(sparkiQ, True)
 
 if __name__ == "__main__":
 
@@ -73,11 +79,15 @@ if __name__ == "__main__":
 	q = Manager().Queue()
 	evidencegrid.launch(0.01, 512, 512, q)
 
+	processes =  Pool(processes=len(comPorts))
+
 	for i in xrange(len(comPorts)):
 		sparki = Sparki(comPorts[i],q)
 		# Set sparkis x and Y here
 		sparki.tellSparkiPos(float(xValues[i]),float(yValues[i]),0)
-		sparkis.put(sparki, True)
+		sparkiQ = Queue()
+		processes.apply_async(loopFunct, (sparkiQ, sparki))
+		sparkis.put(sparkiQ, True)
 
 	if (sparkis.qsize() == 0):
 		print "No Sparki given"
@@ -85,16 +95,14 @@ if __name__ == "__main__":
 
 	generateInitGoals(sparkis.qsize())
 
-	processes =  Pool(processes=len(comPorts))
-
 	while ( notMapped ):
 		try:
-			sparki = sparkis.get(False)
+			sparkiQ = sparkis.get(False)
 		except Empty:
 			pass
 		else:
 			goal = goals.get(True)
-			processes.apply_async(assignTask, (goal, sparki))
+			assignGoal(goal, sparkiQ)
 			time.sleep(1)
 
 	# Waits for all Sparkis to finish
